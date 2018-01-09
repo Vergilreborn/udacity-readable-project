86ad4a774d7bb7ac0a747dc18bb8f4536fd67cab
Alex M <Martinaj192@potsdam.edu>
2018-01-08 20:53:21 -0500

    Adding the sorting to the post list. This should allow the user to sort depending on what content the are looking at

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -2,28 +2,65 @@ import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import {GetDateTimeString} from './../utils/constants';
 import FaMinusCircle from 'react-icons/lib/fa/minus-circle';
+import FaSort from 'react-icons/lib/fa/sort';
+import FaSortDesc from 'react-icons/lib/fa/sort-desc';
+import FaSortAsc from 'react-icons/lib/fa/sort-asc';
 
 class PostList extends Component{
   state = {
+    sortType : null,
+    asc : false,
+    sortString : false
+  }
 
+  sort(event,type, isString = false){
+    event.preventDefault();
+    let {sortType,asc} = this.state;
+    if(sortType === type){
+      asc = !asc;
+    }else{
+      sortType = type;
+      asc = false;
+    }
+
+    this.setState({sortType : sortType, asc: asc,sortString : isString});
+
+  }
+
+  sortRender(type){
+    
+    let {sortType,asc} = this.state;
+    if(sortType === type){
+      return asc ? <FaSortAsc/> : <FaSortDesc/>;
+    }else{
+      return <FaSort/>;
+    }
   }
 
   render(){
 
     const {posts,setupPost,deletePost} = this.props;
-   
+    const {sortType,asc,sortString} = this.state;
+    const visualPosts = posts.slice(0);
+    if(sortType){
+      visualPosts.sort((p1,p2) => sortString ? p2[sortType].localeCompare(p1[sortType]) :  p1[sortType] < p2[sortType] ? -1 : 1);
+      if(!asc){
+        visualPosts.reverse();
+      }
+    }
+    
     return (     
        <div>
            <div>
             <div className="post-column-names">
-              <div className="text">Title</div>
-              <div className="text">Timestamp</div>
-              <div className="count">Rating</div>
-              <div className="count">Comments</div>
+              <div className="text"><a onClick={(e) => this.sort(e,'title',true)}>Title{this.sortRender('title')}</a></div>
+              <div className="text"><a onClick={(e) => this.sort(e,'timestamp')}>Timestamp{this.sortRender('timestamp')}</a></div>
+              <div className="count"><a onClick={(e) => this.sort(e,'voteScore')}>Rating{this.sortRender('voteScore')}</a></div>
+              <div className="count"><a onClick={(e) => this.sort(e,'commentCount')}>Comments{this.sortRender('commentCount')}</a></div>
               <div></div>
             </div>
             {
-              posts.map((post) => 
+              visualPosts.map((post) => 
                 <div key={post.id} className="post">
                   <div className="text"><NavLink to={`/${post.category}/${post.id}`} onClick={()=>setupPost(post.id)}>{post.title}</NavLink></div>
                   <div className="text">{GetDateTimeString(post.timestamp)}</div>
